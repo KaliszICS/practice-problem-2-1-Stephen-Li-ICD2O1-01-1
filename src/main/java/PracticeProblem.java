be5d4f205be04b6a6531bfe6c140ef937fd6a68a
Stephen-Li-ICD2O1-01 <stephen.li@student.tdsb.on.ca>
2026-03-10 15:21:51 +0000

    save

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,35 @@
+/*
+File: Lesson 2.1 - Methods
+Author: Stephen Li
+Date Created: Mar 10, 2026
+Date Last Modified: Mar 10, 2026
+*/
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
-	//Create your functions here
+	public static int sum(int int1, int int2) {
+		int result = int1 + int2;
+		return result;
+	}
+
+	public static int difference(int int1, int int2) {
+		int result = int1 - int2;
+		return result;
+	}
+
+	public static double product(double num1, double num2) {
+		double result = num1 * num2;
+		return result;
+	}
+
+	public static String removeFirst(String word1) {
+		String result = word1.substring(1);
+		return result;
+	}
+
 
 }
